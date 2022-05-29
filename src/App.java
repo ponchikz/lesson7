e844a63a7c0b7db86124b88911d3fe2c0f741c03
Oleg <hrapin@list.ru>
2022-05-29 20:16:59 +0800

    1

--- src/App.java
+++ src/App.java
@@ -2,12 +2,13 @@ public class App {
 
     public static void main(String[] args) {
         Cat cat1 = new Cat("Обжора", 100);
-        Cat cat2 = new Cat("Том", 50);
+        Cat cat2 = new Cat("Том", 55);
         Cat cat3 = new Cat("Балам", 50);
 
         Cat[] cats = {cat1, cat2, cat3};
-        Plate plate = new Plate(300);
+        Plate plate = new Plate(200);//Добавить в тарелку метод, с помощью которого можно было бы добавлять еду в тарелку
 
+        //Создать массив котов и тарелку с едой, попросить всех котов покушать из этой тарелки
         for (int i = 0; i < cats.length; i++) {
             cats[i].eat(plate);
         }
